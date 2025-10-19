a75106d9a4de3a1d8f40d4a73241dd0783e10ab4
YassineElkefi <yassine.elkefi6@gmail.com>
2025-10-19 19:29:44 +0100

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Yassine EL KEFI on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
