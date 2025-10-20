9981f0ca2ba17a4506df95c42a405a811db25be2
YassineElkefi <yassine.elkefi6@gmail.com>
2025-10-20 23:28:13 +0100

    Account View Created

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
     var body: some View {
         NavigationView{
-            Text("Account")
+            Form{
+                Section(header: Text("Personal Info")){
+                    TextField("First Name", text: $firstName)
+                    TextField("Last name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled(true)
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    
+                    Button{
+                        print("save")
+                    }label: {
+                        Text("Save Changes")
+                    }
+                }
+                
+                Section(header: Text("Requests")){
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }
+                .tint(.brandPrimary)
+            }
                 .navigationTitle("⚙️ Account")
         }    }
 }
